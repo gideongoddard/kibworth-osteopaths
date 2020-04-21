ef738bd01a752bbdd2e129f73c2ccd0506fad0aa
Gideon Goddard <goddard.gcw@gmail.com>
2020-04-21 13:41:28 +0100

    Blog templates added. Robots.txt updated to disallow crawlers from crawling it yet.

---
+++ resources/blog-filters-min-20200421.js
@@ -0,0 +1,5 @@
+function showAll(){let cards=document.getElementsByClassName('card');for(let i=0;i<cards.length;i++){cards[i].style.display='grid'}}
+function weeklyTips(){let cards=document.getElementsByClassName('card');let weeklyTipsItems=document.getElementsByClassName('weekly-tip');for(let i=0;i<cards.length;i++){cards[i].style.display='none'}
+for(let i=0;i<weeklyTipsItems.length;i++){weeklyTipsItems[i].style.display='grid'}}
+function anotherCat(){let cards=document.getElementsByClassName('card');let anotherCatItems=document.getElementsByClassName('another-cat');for(let i=0;i<cards.length;i++){cards[i].style.display='none'}
+for(let i=0;i<anotherCatItems.length;i++){anotherCatItems[i].style.display='grid'}}
